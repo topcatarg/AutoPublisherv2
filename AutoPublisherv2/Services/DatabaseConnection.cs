34c333f9d105d8d98c864aaf59fc24375f42f2cd
gbianchi <topcat_arg@yahoo.com>
2021-10-25 18:31:23 -0300

    added database connection

---
+++ AutoPublisherv2/Services/DatabaseConnection.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoPublisherv2.Core;
+
+
+namespace AutoPublisherv2.Services
+{
+    class DatabaseConnection
+    {
+
+        private readonly SqliteConnection db;
+        private readonly string dbpath = Database.dbpath;
+        public DatabaseConnection()
+        {
+            db = new SqliteConnection($"Data Source={dbpath}");
+        }
+
+        ~DatabaseConnection()
+        {
+            try
+            {
+                db.Close();
+                db.Dispose();
+            }
+            catch
+            {
+
+            }
+        }
+
+        
+        public SqliteConnection GetOpenDBConnection()
+        {
+            db.Open();
+            return db;
+        }
+    }
+}
