f193586e9c0b051ce3057ccd5ef036016d243b95
gbianchi <topcat_arg@yahoo.com>
2021-11-02 22:27:09 -0300

    half way publish

---
+++ AutoPublisherv2/Models/Category.cs
@@ -0,0 +1,42 @@
+﻿using AutoPublisherv2.Helpers;
+
+namespace AutoPublisherv2.Models
+{
+    public class Category : INotifyMe
+    {
+
+        private string name;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int id;
+        public int Id
+        {
+            get => id;
+            set
+            {
+                id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool selected = false;
+        public bool Selected
+        {
+            get => selected;
+            set
+            {
+                selected = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
+}
---
+++ AutoPublisherv2/Models/SiteList.cs
@@ -0,0 +1,90 @@
+﻿using AutoPublisherv2.Helpers;
+using System.Collections.ObjectModel;
+
+namespace AutoPublisherv2.Models
+{
+    public class SiteList : INotifyMe
+    {
+        private bool ischecked;
+        public bool IsChecked
+        {
+            get => ischecked;
+            set
+            {
+                ischecked = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string siteurl;
+        public string SiteUrl
+        {
+            get => siteurl;
+            set
+            {
+                siteurl = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool tested = false;
+        public bool Tested
+        {
+            get => tested;
+            set
+            {
+                tested = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool state = false;
+        public bool State
+        {
+            get => state;
+            set
+
+            {
+                state = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int progressvalue = 0;
+        public int ProgressValue
+        {
+            get => progressvalue;
+            set
+            {
+                progressvalue = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string User { get; set; }
+
+        public string Password { get; set; }
+
+        private string message;
+        public string Message
+        {
+            get => message;
+            set
+            {
+                message = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Category> categorylist = new();
+        public ObservableCollection<Category> CategoryList
+        {
+            get => categorylist;
+            set
+            {
+                categorylist = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
