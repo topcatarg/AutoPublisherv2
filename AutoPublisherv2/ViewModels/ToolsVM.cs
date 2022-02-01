20b087b658f804726703007cd4bb39581deca952
gbianchi <topcat_arg@yahoo.com>
2022-02-01 16:40:34 -0300

    not finished commit

---
+++ AutoPublisherv2/ViewModels/ToolsVM.cs
@@ -0,0 +1,112 @@
+﻿using AutoPublisherv2.Helpers;
+using AutoPublisherv2.Models;
+using AutoPublisherv2.Services;
+using FluentFTP;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoPublisherv2.ViewModels
+{
+    class ToolsVM: INotifyMe
+    {
+
+        #region properties
+        private ObservableCollection<SiteList> sites = new ObservableCollection<SiteList>();
+        public ObservableCollection<SiteList> Sites
+        {
+            get => sites;
+            set
+            {
+                sites = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private readonly SiteServices _siteServices;
+
+        public bool ProgressControl
+        { get; set; }
+        #endregion
+
+        private ToolsVM(SiteServices s, ImmutableList<Site> sites)
+        {
+            _siteServices = s;
+            _siteServices.SiteRefreshed += RefreshState;
+            Sites = new ObservableCollection<SiteList>(sites.Select(
+                x => new SiteList()
+                {
+                    Password = x.Password,
+                    SiteUrl = x.SiteURL,
+                    User = x.User
+                }));
+        }
+
+        ~ToolsVM()
+        {
+            _siteServices.SiteRefreshed -= RefreshState;
+        }
+
+        public async static Task<ToolsVM> ClassFactory()
+        {
+
+            SiteServices s = SiteServices.ClassFactory();
+            ToolsVM toolsVM = new(s, await s.SiteList.Value);
+            return toolsVM;
+        }
+
+        public async void RefreshState()
+        {
+            ImmutableList<Site> v = await _siteServices.SiteList.Value;
+            Sites = new ObservableCollection<SiteList>(v.Select(x => new SiteList()
+            {
+                Password = x.Password,
+                SiteUrl = x.SiteURL,
+                User = x.User
+            }));
+        }
+
+        public async Task CheckImages()
+        {
+            ProgressControl = true;
+            string pwd = "Ebj(`h@552$2";
+            string user = "ftpuser@topcatarg.com.ar";
+            string host = "topcatarg.com.ar";
+
+            Uri path = new("ftp://topcatarg.com.ar/topcatarg.com.ar/public_html/wp-content/uploads/");
+            string path2 = "/topcatarg.com.ar/public_html/wp-content/uploads/";
+            using FtpClient client = new(host, user, pwd);
+            await client.ConnectAsync();
+            //_ = await client.AutoConnectAsync();
+            FtpListItem[] filelist = await client.GetListingAsync(path2);
+            List<string> FullFileList = await AllFiles(filelist, client);
+            
+            ProgressControl = false;
+        }
+        private async Task<List<string>> AllFiles(FtpListItem[] Items, FtpClient client)
+        {
+            List<string> Files = new();
+            foreach (FtpListItem file in Items)
+            {
+                if (file.Type == FtpFileSystemObjectType.File)
+                {
+                    Files.Add(file.FullName);
+                }
+                else if (file.Type == FtpFileSystemObjectType.Directory)
+                {
+
+                    FtpListItem[] filelist = await client.GetListingAsync(file.FullName);
+                    List<string> NewFiles = await AllFiles(filelist, client);
+                    Files.AddRange(NewFiles);
+                }
+            }
+            return Files;
+        }
+    }
+
+    
+}
---
+++ AutoPublisherv2/Views/Tools.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections;
+using System.Net;
+using System.IO;
+using FluentFTP;
+using AutoPublisherv2.ViewModels;
+
+namespace AutoPublisherv2.Views
+{
+    /// <summary>
+    /// Lógica de interacción para Tools.xaml
+    /// </summary>
+    public partial class Tools : UserControl
+    {
+
+        private ToolsVM _VM;
+        public Tools()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnCleanImages_Click(object sender, RoutedEventArgs e)
+        {
+
+            await _VM.CheckImages();
+            /*
+            //Uri path = new("ftp://topcatarg.com.ar/");
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(path);
+            request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+            
+            // This example assumes the FTP site uses anonymous logon.
+            request.Credentials = new NetworkCredential(user,pwd);
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            string resul = reader.ReadToEnd();
+
+            //Console.WriteLine($"Directory List Complete, status {response.StatusDescription}");
+
+            reader.Close();
+            response.Close();
+
+            /*
+            var connectionInfo = new ConnectionInfo("topcatarg.com.ar",
+                                        21,
+                                        user,
+                                        new PasswordAuthenticationMethod(user, pwd));
+            
+            using SftpClient client = new SftpClient(connectionInfo);
+            client.Connect();
+            client.ChangeDirectory("/topcatarg.com.ar/public_html/wp-content/uploads");
+            List<Renci.SshNet.Sftp.SftpFile> files = client.ListDirectory("/topcatarg.com.ar/public_html/wp-content/uploads").ToList();
+            */
+        }
+
+        private async void UserControl_Initialized(object sender, EventArgs e)
+        {
+            _VM = await ToolsVM.ClassFactory();
+            DataContext = _VM;
+        }
+    }
+}
